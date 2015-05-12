6dc79a8ad47cf0602bc43b2d0bb7b914fd3c4a01
xrotwang <xrotwang@googlemail.com>
2015-05-12 22:13:17 +0200

    closes #7

--- csd/maps.py
+++ csd/maps.py
@@ -1,4 +1,4 @@
-from clld.web.maps import ParameterMap
+from clld.web.maps import ParameterMap, Map
 
 
 class EntryMap(ParameterMap):
@@ -6,5 +6,11 @@ class EntryMap(ParameterMap):
         return {'show_labels': True, 'max_zoom': 10}
 
 
+class LanguoidsMap(Map):
+    def get_options(self):
+        return {'show_labels': True, 'max_zoom': 10}
+
+
 def includeme(config):
     config.register_map('parameter', EntryMap)
+    config.register_map('languages', LanguoidsMap)
